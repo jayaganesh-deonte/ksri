49c40730ecdf350ccb4b9216100a901a040c6772
Jaya Ganesh Krishna Moorthy <Jaya_Ganesh.Krishna_Moorthy@genesys.com>
2024-11-26 10:08:39 +0530

    add bank info and by post page

--- website/pages/contribute/bank-information.vue
+++ website/pages/contribute/bank-information.vue
@@ -1,3 +1,57 @@
 <template>
-  <div>bank-information</div>
+  <div style="background-color: white" class="ma-4 pa-2">
+    <section-title title="Bank Account Information" />
+    <v-row class="my-4">
+      <v-col cols="12" sm="12" md="5" data-aos="fade-right">
+        <div class="text-h5 font-weight-bold">
+          For local contribution the bank particulars are
+        </div>
+        <div class="text-h6">Bank particulars for local donors</div>
+      </v-col>
+      <v-col cols="12" sm="12" md="7">
+        <!-- add bank Details -->
+
+        <v-table style="background-color: transparent" v-if="!$device.isMobile">
+          <tbody>
+            <tr
+              v-for="(value, key) in bankDetails"
+              :key="key"
+              class="text-h6"
+              data-aos="fade-left"
+              :data-aos-delay="`${bankDetailsKeys.indexOf(key) * 100}`"
+            >
+              <td class="font-weight-bold text-secondary">{{ key }}</td>
+              <td>{{ value }}</td>
+            </tr>
+          </tbody>
+        </v-table>
+
+        <v-list
+          v-else
+          class="text-body-1"
+          data-aos="fade-left"
+          :data-aos-delay="`${bankDetailsKeys.indexOf(key) * 100}`"
+          v-for="(value, key) in bankDetails"
+          :key="key"
+        >
+          <v-list-item>
+            <div class="font-weight-bold text-secondary">{{ key }}</div>
+            <div>{{ value }}</div>
+          </v-list-item>
+        </v-list>
+      </v-col>
+    </v-row>
+  </div>
 </template>
+
+<script setup>
+const bankDetails = {
+  "Our Name": "THE KUPPUSWAMI SASTRI RESEARCH INSTITUTE.",
+  Bank: "Union Bank of India, Mylapore Branch.",
+  "S.B.Account No.": "395702010007408.",
+  "IFSC CODE": "UBIN0539571.",
+  "MICR NO.": "600026009",
+};
+
+const bankDetailsKeys = Object.keys(bankDetails);
+</script>
--- website/pages/contribute/by-post.vue
+++ website/pages/contribute/by-post.vue
@@ -1,3 +1,49 @@
 <template>
-  <div>by-post</div>
+  <div class="">
+    <section-title title="Contribute By Post" />
+    <div class="text-h6 text-center" data-aos="fade-up" data-aos-delay="100">
+      Contributions can also be sent by courier or post by making a cheque or
+      demand draft in the name of The Kuppuswami Sastri Research Institute
+    </div>
+
+    <div class="d-flex justify-center">
+      <v-card
+        color="transparent"
+        class="mt-5 text-center pa-4"
+        elevation="0"
+        rounded="0"
+        :width="$device.isMobile ? '90vw' : '40vw'"
+        data-aos="fade-up"
+        data-aos-delay="200"
+      >
+        <div class="text-h6 font-weight-bold">Send to</div>
+        <div class="text-h5 font-weight-bold mb-4">
+          The Kuppuswami Sastri Research Institute,
+        </div>
+
+        <div class="text-h6 text-secondary font-weight-bold my-2">Address</div>
+        <div class="text-h6 my-2">
+          84, Thiru vi ka Road, Mylapore, Chennai 600 004.
+        </div>
+
+        <div
+          class="d-flex justify-space-around my-4"
+          :class="`${$device.isMobile ? 'flex-column' : 'flex-row'}`"
+        >
+          <div class="d-flex justify-space-between align-center">
+            <div class="text-secondary font-weight-bold text-h6 mx-4">
+              Email
+            </div>
+            <div class="text-h6">ksrinst@gmail.com</div>
+          </div>
+          <div class="d-flex justify-space-between align-center">
+            <div class="text-secondary font-weight-bold text-h6 mx-4">
+              Phone
+            </div>
+            <div class="text-h6">044-24985320</div>
+          </div>
+        </div>
+      </v-card>
+    </div>
+  </div>
 </template>
