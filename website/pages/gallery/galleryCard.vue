a16dfb7cad91588dc82c864d876cd9cdac8f0384
Jaya Ganesh Krishna Moorthy <Jaya_Ganesh.Krishna_Moorthy@genesys.com>
2024-12-01 19:59:26 +0530

    add gallery page

---
+++ website/pages/gallery/galleryCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card
+    class="ma-4"
+    rounded="0"
+    elevation="0"
+    width="300"
+    @mouseover="handleMouseOver"
+    @mouseleave="handleMouseLeave"
+  >
+    <v-img :src="image.imageUrl" fit />
+    <div class="text-body-1">
+      {{ image.description }}
+    </div>
+    <div
+      class="horizontal-line-animate"
+      :class="{ show: isHovering, hide: !isHovering }"
+    ></div>
+  </v-card>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  image: {
+    type: Object,
+    required: true,
+  },
+});
+
+const isHovering = ref(false);
+
+const handleMouseOver = () => {
+  isHovering.value = true;
+};
+
+const handleMouseLeave = () => {
+  isHovering.value = false;
+};
+</script>
+
+<style scoped></style>
--- website/pages/gallery/index.vue
+++ website/pages/gallery/index.vue
@@ -1,3 +1,54 @@
 <template>
-  <div>gallery</div>
+  <div>
+    <div v-for="subsection in gallerySubsections" :key="subsection">
+      <v-card
+        class="mb-6 text-center"
+        rounded="0"
+        elevation="0"
+        color="greenBg"
+      >
+        <v-card-title class="text-h5 font-weight-bold">
+          {{ subsection }}
+        </v-card-title>
+      </v-card>
+
+      <!-- display images -->
+      <div class="d-flex flex-row flex-wrap">
+        <div v-for="image in gallery[subsection]" :key="image">
+          <galleryCard :image="image" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import galleryCard from "./galleryCard.vue";
+
+const gallery = {
+  "KSRI Gallery Collection 1": [
+    {
+      description: "Golden Jubilee Inauguration - 1994",
+      imageUrl: "https://d30y75l38k1y9.cloudfront.net/upload/1-J4t.jpg",
+    },
+    {
+      description: "Sanskrit and Medical science Seminar - On Jan.23, 1995",
+      imageUrl: "https://d30y75l38k1y9.cloudfront.net/upload/3-WNF.jpg",
+    },
+  ],
+  "Memories of KSRI": [
+    {
+      description:
+        "Inaugration of the KSRInstitute by S.V.Ramamurthi I.C.S. on 22nd April 1945. L.to R.: K.M.Munshi, Sir.S.V.Ramamurthi, Rt.Hon'ble V.S. Srinivasa Sastri & Dr. S. Radhakrishnan",
+      imageUrl: "https://d30y75l38k1y9.cloudfront.net/upload/1.jpg",
+    },
+    {
+      description:
+        "H.H. Jaya Chamarajendra Wadiyar, Maharajah of Mysore, at the Institute, seeing the Library. 1st September 1957",
+      imageUrl: "https://d30y75l38k1y9.cloudfront.net/upload/3.jpg",
+    },
+  ],
+};
+
+const gallerySubsections = Object.keys(gallery);
+</script>
