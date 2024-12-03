9ceb7187b4c09ee9d0df249a48cbdb8e46600919
Jaya Ganesh Krishna Moorthy <Jaya_Ganesh.Krishna_Moorthy@genesys.com>
2024-12-03 12:33:34 +0530

    add ksri associates

--- website/pages/academic-and-research-pursuits/ksri-associates.vue
+++ website/pages/academic-and-research-pursuits/ksri-associates.vue
@@ -1,3 +1,41 @@
 <template>
-  <div>ksri-associates</div>
+  <div>
+    <div class="my-8">
+      <section-title title="KSRI ASSOCIATIONS" />
+      <div class="sectionSubtitle2" data-aos="fade-up">
+        The KSRI is associated with many Indian Organizations & Foreign
+        Universities
+      </div>
+    </div>
+    <div class="text-h6 text-center" data-aos="fade-up">
+      The KSRI is associated closely with many organizations such as Madras
+      Sanskrit College, Sanskrit Academy, Samskrita Ranga, All India Oriental
+      Conference, Rashtriya Sanskrit Sansthan, New Delhi University of Madras
+      and so on.
+    </div>
+    <v-row class="ma-4" align="center">
+      <v-col data-aos="fade-right" cols="12" md="6">
+        <v-img
+          src="https://d30y75l38k1y9.cloudfront.net/upload/gal-img-1.png"
+          cover
+        ></v-img>
+      </v-col>
+      <v-col class="text-h6" cols="12" md="6">
+        <div class="my-6" data-aos="fade-left" data-aos-delay="200">
+          Among the foreign Universities, there have been many from across the
+          globe from where the scholars have been affiliated with us. Recently
+          the University of Oslo has also tied up with KSRI for training their
+          students in Sanskrit.
+        </div>
+        <div data-aos="fade-left" data-aos-delay="400">
+          Some of the other foreign universities are: Colgate University,
+          University of Pennsylvania, University of Chicago, Elon University,
+          Sorbonne University, Oxford University. Yale University, Concordia
+          University, Mcgill University, Mcmaster University, Harvard
+          University, University of California,Columbia University, University
+          of Michigan etc.
+        </div>
+      </v-col>
+    </v-row>
+  </div>
 </template>
