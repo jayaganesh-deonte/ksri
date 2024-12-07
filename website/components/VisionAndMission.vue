2366fa5bc673940fe5982e43badc68444651dcdf
Jaya Ganesh Krishna Moorthy <Jaya_Ganesh.Krishna_Moorthy@genesys.com>
2024-12-07 12:35:52 +0530

    update seo location for vision and mission

--- website/components/VisionAndMission.vue
+++ website/components/VisionAndMission.vue
@@ -99,18 +99,6 @@
 </template>
 
 <script setup>
-const description = `We at KSRI "PRESERVE" the knowledge of Sanskrit and related Indology,
-  We at KSRI in pursuit of our stated vision will strive to make the Institute a preferred centre of learning in India for Sanskrit language and related Indology.`;
-
-useSeoMeta({
-  title: "Vision and Mission",
-  description: description,
-  ogTitle: "Vision and Mission",
-  ogDescription: description,
-  twitterTitle: "Vision and Mission",
-  twitterDescription: description,
-});
-
 const missionPoints = [
   "Original research",
   "Propagation",
