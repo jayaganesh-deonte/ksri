61b7a2079d3e8afff287a2ad3c7b12cbf94b87c2
Jaya Ganesh Krishna Moorthy <Jaya_Ganesh.Krishna_Moorthy@genesys.com>
2025-03-27 10:02:56 +0530

    add search

--- website/components/FloatingIcon.vue
+++ website/components/FloatingIcon.vue
@@ -2,6 +2,7 @@
   <div class="floating-icons-panel">
     <v-card elevation="2" class="icon-panel" rounded="0">
       <v-list dense style="background-color: rgb(191, 100, 31, 0.5)">
+        <search />
         <template v-for="(link, index) in socialMediaLinks" :key="link.name">
           <v-list-item :href="link.url" target="_blank" class="icon-item">
             <v-list-item-icon>
