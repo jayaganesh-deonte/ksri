5e765aadd0e2c24f2b3a4d769de71d8522ad00bd
Jaya Ganesh Krishna Moorthy <Jaya_Ganesh.Krishna_Moorthy@genesys.com>
2025-03-27 10:06:00 +0530

    fix position of marquee

--- website/components/marqueeTexts.vue
+++ website/components/marqueeTexts.vue
@@ -1,20 +1,21 @@
 <template>
-  <v-system-bar v-if="store.marqueeTexts.length > 0">
+  <v-card
+    v-if="store.marqueeTexts.length > 0"
+    color="transparent"
+    elevation="0"
+    rounded="0"
+  >
     <NuxtMarquee pause-on-hover pause-on-click autoFill>
       <div v-for="(marqueeText, index) in store.marqueeTexts" :key="index">
         <!-- display text and add link to it -->
-        <NuxtLink
-          :to="marqueeText.link"
-          class="mx-8"
-          style="text-decoration: unset"
-        >
+        <NuxtLink :to="marqueeText.link" style="text-decoration: unset">
           <div class="text-primary mx-8 font-weight-bold text-h6">
             {{ marqueeText.name }}
           </div>
         </NuxtLink>
       </div>
     </NuxtMarquee>
-  </v-system-bar>
+  </v-card>
 </template>
 
 <script setup>
