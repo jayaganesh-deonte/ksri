43e94da130f6c6db9102aeca0d648b60ca256cb8
Jaya Ganesh Krishna Moorthy <Jaya_Ganesh.Krishna_Moorthy@genesys.com>
2025-06-09 16:56:23 +0530

    add envs

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -32,17 +32,17 @@ export default defineNuxtConfig({
     REDIRECT_URL: process.env.REDIRECT_URL,
     public: {
       REGION: "ap-south-1",
-      API_URL: "https://preview.ksri.in/public",
-      PURCHASE_API_URL: "https://preview.ksri.in/e-v1",
+      API_URL: "https://ksri.in/public",
+      PURCHASE_API_URL: "https://ksri.in/e-v1",
       // PURCHASE_API_URL: "http://localhost:3002/e-v1",
       LAMBDA_URL_DOMAIN:
-        "v3ed4tsg4nwps7kpcr2pmsnedu0yujpz.lambda-url.ap-south-1.on.aws",
-      CLOUDFRONT_DOMAIN: "preview.ksri.in",
+        "4eex5bnwjectmzbnzcuccamf7a0ctqhx.lambda-url.ap-south-1.on.aws",
+      CLOUDFRONT_DOMAIN: "ksri.in",
       // CLOUDFRONT_DOMAIN: "localhost:3002",
       // API_URL: "http://localhost:3001/public",
       CONTACT_US_URL:
-        "https://k6k5z57jue2xojyecp4dvmab640dxzeb.lambda-url.ap-south-1.on.aws/",
-      ASSET_DOMAIN: "https://d3dx8df9hmf5nm.cloudfront.net/",
+        "https://4fhxozuh262x5cgohmdstdvmfm0rvmef.lambda-url.ap-south-1.on.aws/",
+      ASSET_DOMAIN: "https://d1tkn0t59vli2s.cloudfront.net/",
       WORKING_KEY: process.env.WORKING_KEY,
       ACCESS_CODE: process.env.ACCESS_CODE,
       MERCHANT_ID: process.env.MERCHANT_ID,
